25d76d1148e25341f50fd664d1bde4dd8c096d53
Matthew DiBello <matthew.quickbeam@gmail.com>
2017-12-07 21:38:29 -0500

    Completed DFA

--- src/dfa.rb
+++ src/dfa.rb
@@ -2,17 +2,26 @@
 # Interpreter for DFAs
 # Matthew DiBello
 
-Struct.new("DFA", :Q, :sigma, :delta, :q0, :F) do
+class DFA
+
+  def initialize states, sigma, delta, q0, final
+    @Q = states
+    @sigma = sigma
+    @delta = delta
+    @q0 = q0
+    @F = final
+  end
 
   def accepts w
 
     current_state = @q0
 
     w.split('').each do |c|
+      return false if !@sigma.include? c
       current_state = @delta[[current_state, c]]
     end
 
-    return true if @F.contains current_state
+    return true if @F.include? current_state
     return false
 
   end
--- test/dfa-test.rb
+++ test/dfa-test.rb
@@ -5,18 +5,44 @@ class DFA_TEST < Test::Unit::TestCase
 
   def setup
 
-    @even_length = Struct::DFA.new(
+    @even_length = DFA.new(
       [:a, :b],
-      [0, 1],
-      { [:a, 0] => :b, [:a, 1] => :b, [:b, 0] => :a, [:b, 1] => :a },
+      ['0', '1'],
+      { [:a, '0'] => :b, [:a, '1'] => :b, [:b, '0'] => :a, [:b, '1'] => :a },
       :a,
-      [:b]
+      [:a]
+    )
+
+    @even_value = DFA.new(
+      [:x, :y],
+      ['0', '1'],
+      { [:x, '1'] => :x, [:x, '0'] => :y, [:y, '0'] => :y, [:y, '1'] => :x },
+      :x,
+      [:y]
     )
 
   end
 
   def test_fail
-    assert(@even_length.accepts(""), "Incorrectly rejects even-length \"\"")
+
+    a = ['', '01', '1011', '1111111111111100']
+    r = ['1', '111', '00001', '1110001110001', 'kk', 'uu11']
+    a.each do |w|
+      assert(@even_length.accepts(w), "Incorrectly rejects even-length \"#{w}\"")
+    end
+    r.each do |w|
+      assert(@even_length.rejects(w), "Incorrectly accepts even-length \"#{w}\"")
+    end
+
+    a = ['0', '10', '000', '111111110']
+    r = ['', '1', '11', '111', '100000001']
+    a.each do |w|
+      assert(@even_value.accepts(w), "Incorrectly rejects even-value \"#{w}\"")
+    end
+    r.each do |w|
+      assert(@even_value.rejects(w), "Incorrectly accepts even-value \"#{w}\"")
+    end
+
   end
 
 end
