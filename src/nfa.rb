49785459b674dc3e9701f9d70f16ec65aa7ae89d
Matthew DiBello <matthew.quickbeam@gmail.com>
2017-12-09 15:42:57 -0500

    Completed epsilon function]

--- src/nfa.rb
+++ src/nfa.rb
@@ -4,31 +4,52 @@
 
 require_relative './util.rb'
 
+def epsilon_function delta, new_delta, q, c, v
+  if v.kind_of? Array
+    v.each { |w| epsilon_function delta, new_delta, q, c, w }
+  else
+    if delta.has_key? [v, :e]
+      x = delta[[v, :e]]
+      if (x.kind_of?(Symbol) && !new_delta[[q, c]].include?(x)) ||
+         (x.kind_of?(Array) && !(new_delta[[q, c]] & x).empty?)
+        new_delta[[q, c]].push x
+        epsilon_function delta, new_delta, q, c, x
+      end
+    end
+  end
+end    
+
 class NFA
 
   def initialize states, sigma, delta, q0, final
 
     new_delta = {}
     (powerset states).each do |q|
+      q.sort!
       sigma.each do |c|
         new_delta[[q, c]] = []
         q.each do |x|
-          new_delta[[q, c]].push delta[[x, c]] if delta.has_key? [x, c]
+          if delta.has_key? [x, c]
+            v = delta[[x, c]]
+            new_delta[[q, c]].push v
+            epsilon_function delta, new_delta, q, c, v
+          end
         end
         if new_delta[[q, c]] == []
           new_delta.delete [q, c]
         else
           new_delta[[q, c]].flatten!
+          new_delta[[q, c]].sort!
         end
       end
     end
-      
+
     @DFA = DFA.new(
       (powerset states),
       sigma,
       new_delta,
       [q0],
-      (powerset states).select { |q| q & final != [] }
+      (powerset states).select { |q| q & final != [] }.map { |x| x.sort }
     )
 
   end
--- test/nfa-test.rb
+++ test/nfa-test.rb
@@ -13,17 +13,34 @@ class NFA_TEST < Test::Unit::TestCase
       [:a]
     )
 
+    @odd_length = NFA.new(
+      [:q, :r, :s],
+      ['0', '1'],
+      { [:q, '0'] => :s, [:q, '1'] => :r, [:r, '0'] => :q, [:r, :e] => :s, [:s, :e] => :r, [:s, '1'] => :q },
+      :q,
+      [:s]
+    )
+
   end
 
   def test_fail
 
-    a = ['111', '111', '10111', '1111111111111110', '00000000000000100']
+    a = ['111', '10111', '1111111111111110', '00000000000000100']
     r = ['', '1', '011', '00001', '1110001110001', 'kk', 'uu11']
     a.each do |w|
       assert(@one_third_last.accepts(w), "Incorrectly rejects one_third_last \"#{w}\"")
     end
     r.each do |w|
       assert(@one_third_last.rejects(w), "Incorrectly accepts one_third_last \"#{w}\"")
+    end
+
+    a = ['0', '1', '101', '1111111111111']
+    r = ['', '01', 'as', 'dsf', '11111111']
+    a.each do |w|
+      assert(@odd_length.accepts(w), "Incorrectly rejects odd_length \"#{w}\"")
+    end
+    r.each do |w|
+      assert(@odd_length.rejects(w), "Incorrectly accepts odd_length \"#{w}\"")
     end
 
   end
